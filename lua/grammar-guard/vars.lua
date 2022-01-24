be77e7505fd962c739843f2a2114d59258b063c5
yorik1984 <yorik1984@gmail.com>
2022-01-25 00:00:13 +0200

    change default install path

--- lua/grammar-guard/vars.lua
+++ lua/grammar-guard/vars.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 -- Using nvim-lsp-installer path
-M.install_path = vim.fn.stdpath("data") .. "/lsp_servers/ltex"
+M.install_path = vim.fn.stdpath("data") .. "/lsp_servers/ltex/ltex-ls/bin"
 
 if vim.fn.has("win32") == 1 then
     M.bin = "/ltex-ls.bat"
